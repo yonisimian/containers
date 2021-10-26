9f4a1f2ab55f84d4bb05d1c003ccecbc5deb9726
Yehonatan Simian <73541314+yonisimian@users.noreply.github.com>
2021-10-26 22:48:22 +0300

    Create queue.h

---
+++ C/queue.h
@@ -0,0 +1,28 @@
+#ifndef QUEUE_H_
+#define QUEUE_H_
+
+#include <stdbool.h>
+
+typedef void* Element;
+typedef Element (*ElemCopyFunction)(Element);
+typedef void (*ElemFreeFunction)(Element);
+
+typedef struct queue* Queue;
+
+typedef enum {
+    QUEUE_SUCCESS,
+    QUEUE_OUT_OF_MEMORY,
+    QUEUE_NULL_ARG,
+    QUEUE_IS_EMPTY
+} QueueResult;
+
+Queue queueCreate(ElemCopyFunction, ElemFreeFunction);
+Queue queueCopy(Queue queue);
+void queueDestroy(Queue queue);
+QueueResult queueEnqueue(Queue queue, Element element);
+QueueResult queueDequeue(Queue queue);
+QueueResult queueTop(Queue queue, Element* element);
+int queueGetSize(Queue queue);
+bool queueIsEmpty(Queue queue);
+
+#endif /* QUEUE_H_ */
