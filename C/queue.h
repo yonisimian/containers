3ce3f36fec860f7cb63aa4800c95e1632ed1659e
Yehonatan Simian <73541314+yonisimian@users.noreply.github.com>
2021-10-27 00:07:19 +0300

    Update queue.h

--- C/queue.h
+++ C/queue.h
@@ -7,8 +7,7 @@ typedef void* Element;
 typedef Element (*ElemCopyFunction)(Element);
 typedef void (*ElemFreeFunction)(Element);
 
-typedef struct queue* Queue;
-
+typedef struct queue_t* Queue;
 typedef enum {
     QUEUE_SUCCESS,
     QUEUE_OUT_OF_MEMORY,
@@ -21,7 +20,7 @@ Queue queueCopy(Queue queue);
 void queueDestroy(Queue queue);
 QueueResult queueEnqueue(Queue queue, Element element);
 QueueResult queueDequeue(Queue queue);
-QueueResult queueTop(Queue queue, Element* element);
+QueueResult queueFront(Queue queue, Element* element);
 int queueGetSize(Queue queue);
 bool queueIsEmpty(Queue queue);
 
